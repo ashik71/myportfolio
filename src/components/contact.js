9616f24c254751e638697f5eb09c2d3eb934934f
ashik71 <a.ashrafe71@gmail.com>
2019-05-11 14:32:16 +0600

    update

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,13 +1,49 @@
 import React, {Component} from 'react';
 
-class Contact extends Component{
-    render(){
-        return(
-            <div>
-                <h1>Contact Page</h1>
-            </div>
-        )
-    }
-}
+import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl';
 
+
+class Contact extends Component {
+  render() {
+    return(
+      <div className="contact-body">
+        <Grid className="contact-grid">
+          <Cell col={6}>
+            <h2>Md Ashik Ashrafe</h2>
+            <img
+              src="images/Profile.jpg"
+              alt="avatar"
+              style={{height: '250px',borderRadius:'2%'}}
+               />
+             <p style={{ width: '75%', margin: 'auto', paddingTop: '1em'}}>
+             proactive self-starter who easily adapts into new environments.
+             To succeed in an environment of growth and excellence. earn a job which provides
+             me job Satisfaction and self-development and helps me to achieve personal as well as organization goals.
+             </p>
+          </Cell>
+          <Cell col={6}>
+            <h2>Contact Me</h2>
+            <hr/>
+            <div className="contact-list">
+              <List>
+                <ListItem>
+                  <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
+                    <i className="fa fa-linkedin" aria-hidden="true"/>                    
+                    /ashik-ashrafe
+                  </ListItemContent>
+                </ListItem>               
+                <ListItem>
+                  <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
+                    <i className="fa fa-envelope" aria-hidden="true"/>
+                    a.ashrafe71@gmail.com
+                  </ListItemContent>
+                </ListItem>               
+              </List>
+            </div>
+          </Cell>
+        </Grid>
+      </div>
+    )
+  }
+}
 export default Contact;
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,11 +1,100 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { Tab, Tabs, Grid, Cell, Card, CardMenu, CardTitle, CardText, CardActions, Button, Icon, IconButton } from 'react-mdl';
 
-class Projects extends Component{
-    render(){
-        return(
-            <div>
-                <h1>Projects Page</h1>
+class Projects extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { activeTab: 0 };
+    }
+
+    toggoleTabs = () => {
+        if (this.state.activeTab === 0) {
+            return (
+                <div className="projects-grid">
+                    <div class="card border-success mb-3" style={{ maxWidth: '18rem' }}>
+                        <div class="card-header bg-transparent border-success">Header</div>
+                        <div class="card-body text-success">
+                            <h5 class="card-title">Success card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                        <div class="card-footer bg-transparent border-success">Footer</div>
+                    </div>
+                </div>
+
+            )
+        }
+        else if (this.state.activeTab === 1) {
+            return (
+                <div className="projects-grid">
+                    {/* Project 1 */}
+                    <Card className="hoverCard" shadow={5} style={{ minWidth: '450', margin: 'auto',border:'1px solid red' }}>
+                        <CardTitle className="image" style={{ color: '#fff', height: '176px', textTransform: "capitalize", background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover' }} >phone store #1</CardTitle>
+                        <CardText>
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+                  </CardText>
+                  <CardText className="middle">
+                  <div class="text">Under Development</div>
+                  </CardText>
+                        <CardActions border>
+                            <Button colored>GitHub</Button>
+                            <Button colored>CodePen</Button>
+                            <Button colored disabled>Live Demo</Button>
+                        </CardActions>
+                        <CardMenu style={{ color: '#fff' }}>
+                            <IconButton name="share" />
+                        </CardMenu>
+                    </Card>
+                    {/* Project 2 */}
+                    <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
+                        <CardTitle style={{ color: '#fff', height: '176px', background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover' }} >React Project #2</CardTitle>
+                        <CardText>
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+                  </CardText>
+                        <CardActions border>
+                            <Button colored>GitHub</Button>
+                            <Button colored>CodePen</Button>
+                            <Button colored>Live Demo</Button>
+                        </CardActions>
+                        <CardMenu style={{ color: '#fff' }}>
+                            <IconButton name="share" />
+                        </CardMenu>
+                    </Card>
+                    {/* Project 3 */}
+                    <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
+                        <CardTitle style={{ color: '#fff', height: '176px', background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover' }} >React Project #3</CardTitle>
+                        <CardText>
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+                  </CardText>
+                        <CardActions border>
+                            <Button colored>GitHub</Button>
+                            <Button colored>CodePen</Button>
+                            <Button colored>Live Demo</Button>
+                        </CardActions>
+                        <CardMenu style={{ color: '#fff' }}>
+                            <IconButton name="share" />
+                        </CardMenu>
+                    </Card>
+                </div>
+            )
+
+        }
+    }
+    render() {
+        return (
+            <div className="category-tabs">
+                <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
+                    <Tab>ASP.NET</Tab>
+                    <Tab>React</Tab>
+                </Tabs>
+                <section className="projects-grid">
+                    <Grid className="projects-grid">
+                        <Cell col={12}>
+                            <div className="content">{this.toggoleTabs()}</div>
+                        </Cell>
+                    </Grid>
+                </section>
             </div>
+
         )
     }
 }
